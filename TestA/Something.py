6277187cdf6adc39b4340f3e42141aea9ce84d22
Hydoto <33195895+Hydoto@users.noreply.github.com>
2019-03-07 11:17:24 +0100

    well it works

--- TestA/Something.py
+++ TestA/Something.py
@@ -1,8 +1,13 @@
 from random import randint
 
 
+def main():
+    first = randint(0,4)
 
-first = randint(0,4)
+    if first < 2:
+        print("Hello me")
 
-if first < 2:
-    print("Hello me")
+
+if __name__ == '__main__':
+    main()
+    
